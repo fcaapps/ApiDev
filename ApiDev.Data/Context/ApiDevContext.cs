f867bd5e8be4c7d18ac66abbbaa26c19d1e5946e
Fernando de Castro <fca.apps@gmail.com>
2021-07-19 11:42:37 -0300

    Criação entidade FORNECEDOR e persistencia no banco de dados via MIGRATION

---
+++ ApiDev.Data/Context/ApiDevContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using CrudFornec.Data.Extensions;
+//using CrudFornec.Data.Mappings;
+using ApiDev.Domain.Entities;
+
+
+namespace ApiDev.Data.Content
+{
+    public class ApiDevContext : DbContext
+    {
+        public ApiDevContext(DbContextOptions<ApiDevContext> option)
+            : base(option) {}
+
+        #region "DbSets"
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserMap());
+
+            //modelBuilder.ApplyGlobalConfigurations();
+            //modelBuilder.SeedData();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- ApiDev/Startup.cs
+++ ApiDev/Startup.cs
@@ -1,6 +1,8 @@
+using ApiDev.Data.Content;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace ApiDev
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<ApiDevContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("TemplateDB")).EnableSensitiveDataLogging());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
